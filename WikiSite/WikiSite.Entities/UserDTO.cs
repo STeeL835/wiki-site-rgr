66617d944e58d23ed5e75a59dd2387644847046f
Дмитрий Фролов <steel835@gmail.com>
2017-03-27 23:54:10 +0400

    Initial commit

---
+++ WikiSite/WikiSite.Entities/UserDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikiSite.Entities
+{
+    public class Class1
+    {
+    }
+}
