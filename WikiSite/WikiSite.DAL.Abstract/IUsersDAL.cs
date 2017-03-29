cf6aceac325d54c0bf6e68e476e801ac009c18ca
Дмитрий Фролов <steel835@gmail.com>
2017-03-29 19:52:53 +0400

    Added docs for IUsersDAL

--- WikiSite/WikiSite.DAL.Abstract/IUsersDAL.cs
+++ WikiSite/WikiSite.DAL.Abstract/IUsersDAL.cs
@@ -9,15 +9,59 @@ namespace WikiSite.DAL.Abstract
 {
     public interface IUsersDAL
     {
+		/// <summary>
+		/// Adds user to a database
+		/// </summary>
+		/// <remarks>
+		/// This method doesn't count SmallID field since it 
+		/// managed by an SQL Database.
+		/// </remarks>
+		/// <param name="user">User DTO</param>
 	    void AddUser(UserDTO user);
-	    void UpdateUser(UserDTO user);
+		/// <summary>
+		/// Updates user info in a database
+		/// </summary>
+		/// <remarks>
+		/// This method doesn't count SmallID field since it 
+		/// managed by an SQL Database.
+		/// </remarks>
+		/// <param name="user">User DTO with the same ID and new data</param>
+		void UpdateUser(UserDTO user);
+		/// <summary>
+		/// Removes user from a database.
+		/// </summary>
+		/// <param name="userId">GUID of user to delete</param>
 	    void RemoveUser(Guid userId);
 
+		/// <summary>
+		/// Gets all users from database
+		/// </summary>
+		/// <returns>Users' DTOs</returns>
 	    IEnumerable<UserDTO> GetUsers();
-	    IEnumerable<UserDTO> GetUsers(Guid roleId);
+		/// <summary>
+		/// Gets all users from database with a certain role
+		/// </summary>
+		/// <param name="roleId">GUID of a role</param>
+		/// <returns>Users' DTOs</returns>
+		IEnumerable<UserDTO> GetUsers(Guid roleId);
+		/// <summary>
+		/// Gets a certain user from a database
+		/// </summary>
+		/// <param name="userId">GUID of user to get</param>
+		/// <returns>DTO of a user</returns>
 	    UserDTO GetUser(Guid userId);
-	    UserDTO GetUser(int userShortId);
+		/// <summary>
+		/// Gets a certain user from a database
+		/// </summary>
+		/// <param name="userShortId">Incremental ID (number) of user to get</param>
+		/// <returns>DTO of a user</returns>
+		UserDTO GetUser(int userShortId);
 
+		/// <summary>
+		/// Searches for the user in database by it's nickname
+		/// </summary>
+		/// <param name="searchInput">Search string</param>
+		/// <returns>Collection of users whose nickname matches the criteria</returns>
 	    IEnumerable<UserDTO> SearchUsers(string searchInput);
     }
 }
