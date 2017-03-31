4a6d6f1595698826dbdcfd8c3f6a85fa2cb03bb8
Дмитрий Фролов <steel835@gmail.com>
2017-04-01 00:50:13 +0400

    Added Provider to a TLA

---
+++ WikiSite/WikiSite.DI.Provider/Provider.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Configuration;
+using WikiSite.BLL.Abstract;
+using WikiSite.BLL.Default;
+using WikiSite.DAL.Abstract;
+using WikiSite.DAL.SQL;
+
+namespace WikiSite.DI.Provider
+{
+    public class Provider
+    {
+		public static IUsersDAL UserDAO { get; private set; }
+		public static IUserCredentialsDAL CredentialsDAO { get; private set; }
+
+		public static IUsersBLL UserBLO { get; private set; }
+		public static IUserCredentialsBLL CredentialsBLO { get; private set; }
+
+		static Provider()
+		{
+			string dal;
+			try
+			{
+				dal = ConfigurationManager.AppSettings["DAL"];
+			}
+			catch (Exception e)
+			{
+				throw new ApplicationException("Incorrect configuration file. DAL key not found", e);
+			}
+
+			string bll;
+			try
+			{
+				bll = ConfigurationManager.AppSettings["BLL"];
+			}
+			catch (Exception e)
+			{
+				throw new ApplicationException("Incorrect configuration file. BLL key not found", e);
+			}
+
+			SetCurrentDAL(dal);
+			SetCurrentBLL(bll);
+		}
+
+		private static void SetCurrentDAL(string configValue)
+		{
+			switch (configValue.ToUpperInvariant())
+			{
+				case "SQL":
+					UserDAO = new WikiSite.DAL.SQL.UsersDAO();
+					CredentialsDAO = new WikiSite.DAL.SQL.UserCredentialsDAO();
+					break;
+				default:
+					throw new ApplicationException($"Incorrect configuration file. Inconsistent [DAL] key value: {configValue}");
+			}
+		}
+
+		private static void SetCurrentBLL(string configValue)
+		{
+			switch (configValue.ToUpperInvariant())
+			{
+				case "DEFAULT":
+					UserBLO = new UsersBLO(UserDAO);
+					CredentialsBLO = new UserCredentialsBLO(CredentialsDAO);
+					break;
+				default:
+					throw new ApplicationException($"Incorrect configuration file. Inconsistent [BLL] key value: {configValue}");
+			}
+		}
+	}
+}
+}
---
+++ WikiSite/WikiSite.PL.Console/PromptHelper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace WikiSite.PL.Console
+{
+	public static class PromptHelper
+	{
+		public static Guid PromptGuid(string hint, string errorHint = null)
+		{
+			Guid result;
+			bool success;
+			do
+			{
+				System.Console.Write(hint);
+				success = Guid.TryParse(System.Console.ReadLine(), out result);
+				if (!success && errorHint != null) System.Console.WriteLine(errorHint + Environment.NewLine);
+			} while (!success);
+			return result;
+		}
+
+		public static DateTime PromptDateTime(string hint, Predicate<DateTime> additionalCondition, string errorHint = null)
+		{
+			DateTime result;
+			bool success;
+			do
+			{
+				System.Console.Write(hint);
+				success = DateTime.TryParse(System.Console.ReadLine(), out result);
+				if ((!success || additionalCondition(result)) && errorHint != null)
+					System.Console.WriteLine(errorHint + Environment.NewLine);
+			} while (!success || additionalCondition(result));
+			return result;
+		}
+
+		public static string PromptString(string hint, string errorHint = null)
+		{
+			string result;
+			bool success;
+			do
+			{
+				System.Console.Write(hint);
+				result = System.Console.ReadLine();
+				success = !string.IsNullOrWhiteSpace(result);
+				if (!success && errorHint != null)
+					System.Console.WriteLine(errorHint + Environment.NewLine);
+			} while (!success);
+			return result.Trim();
+		}
+
+		public static int PromptInt(string hint, Predicate<int> additionalCondition, string errorHint = null)
+		{
+			int result;
+			bool success;
+			do
+			{
+				System.Console.Write(hint);
+				success = int.TryParse(System.Console.ReadLine(), out result);
+				if ((!success || additionalCondition(result)) && errorHint != null)
+					System.Console.WriteLine(errorHint + Environment.NewLine);
+			} while (!success && additionalCondition(result));
+			return result;
+		}
+	}
+}
