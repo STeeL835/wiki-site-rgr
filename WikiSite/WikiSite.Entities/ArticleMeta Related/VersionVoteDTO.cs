2efa47e9f8d4919ab4006569890d3a836a03547d
Дмитрий Фролов <steel835@gmail.com>
2017-05-27 19:05:49 +0400

    VersionVotes DAO candidate (not checked yet)

---
+++ WikiSite/WikiSite.Entities/ArticleMeta Related/VersionVoteDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WikiSite.Entities.ArticleMeta_Related
+{
+	public class VersionVoteDTO
+	{
+		public Guid Id { get; set; }
+		public Guid ArticleVersionId { get; set; }
+		public Guid UserId { get; set; }
+		public bool Vote { get; set; }
+	}
+}
