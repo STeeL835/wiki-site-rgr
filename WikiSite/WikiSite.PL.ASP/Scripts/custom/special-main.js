0d702d01ed579a3e01816b3d0d83d8de150eae07
Eugene Smirnov <theproteks.business@gmail.com>
2018-02-26 23:29:48 +0400

    BUG IS DEFEATED! OMG, i'm tired...
    # If you try to upload not image/* file, submit button will be disabled.

--- WikiSite/WikiSite.PL.ASP/Scripts/custom/special-main.js
+++ WikiSite/WikiSite.PL.ASP/Scripts/custom/special-main.js
@@ -1,31 +1,32 @@
-﻿$(document).ready(function () {
-    function updateSize() {
-        var file = document.getElementById("uploadInput").files[0],
-            str = "";
-        if (!file.type.match("image.*")) {
-            str = "Не-не-не, только картинки";
-            document.getElementById('outputImage').src = "http://project-wikisite-835.azurewebsites.net/Image/Get?id=00000000-0000-0000-0000-000000000000";
-        }
-        document.getElementById("exepthionInfo").innerHTML = [str].join("<br>");
-    }
-
-    function handleFileSelect(evt) {
-        var files = evt.target.files,
-            file = file[0];
-        if (!file.type.match("image.*")) {
-            str = "Не-не-не, только картинки";
-            //document.getElementById('outputImage').src = "/Image/Get?id=00000000-0000-0000-0000-000000000000";
-            document.getElementById('exepthionInfo').innerHTML = [str].join('');
-            files.file[0] = null;
-        } else {
-            var reader = new FileReader();
-            reader.onload = (function (theFile) {
-                return function (e) {
-                    document.getElementById('outputImage').src = e.target.result;
-                };
-            })(f);
-            reader.readAsDataURL(file);
-        }
-    }
-    document.getElementById('uploadInput').addEventListener('change', handleFileSelect, false);
+﻿$(document).ready(function () { 
+    var outputImage = document.getElementById('outputImage'), 
+        uploadInput = document.getElementById('uploadInput'), 
+        exepthionInfo = document.getElementById('exepthionInfo'), 
+        imageHeader = document.getElementById('imageHeader'),
+        submitButton = document.getElementById('submitButton'); 
+    function handleFileSelect(evt) { 
+        var files = evt.target.files, 
+            file = files[0]; 
+        if (!file.type.match("image/*")) { 
+            str = "Не-не-не, только картинки"; 
+            exepthionInfo.innerHTML = [str].join('');
+            //outputImage.src = "/Image/Get?id=00000000-0000-0000-0000-000000000000";  
+            imageHeader.classList.remove('text-success');
+            imageHeader.classList.add('text-danger'); 
+            submitButton.setAttribute('disabled', true);
+        } else { 
+            var reader = new FileReader(); 
+            reader.onload = (function (theFile) { 
+                return function (e) { 
+                    outputImage.src = e.target.result; 
+                }; 
+            })(file); 
+            reader.readAsDataURL(file); 
+            exepthionInfo.innerHTML = [].join(''); 
+            imageHeader.classList.add('text-success');
+            imageHeader.classList.remove('text-danger');
+            submitButton.removeAttribute('disabled');
+        } 
+    } 
+    uploadInput.addEventListener('change', handleFileSelect, false); 
 });
