8c680c55f1d7608138b9765d34198bfbcd76a11c
OniOnyMan <theproteks.business@gmail.com>
2018-02-26 21:11:43 +0400

    Bug fixes. Aprooving preview of uploading image.

--- WikiSite/WikiSite.PL.ASP/Scripts/custom/special-main.js
+++ WikiSite/WikiSite.PL.ASP/Scripts/custom/special-main.js
@@ -1,14 +1,31 @@
-﻿$(document).ready(function() {
+﻿$(document).ready(function () {
     function updateSize() {
         var file = document.getElementById("uploadInput").files[0],
             str = "";
         if (!file.type.match("image.*")) {
             str = "Не-не-не, только картинки";
-            document.getElementById('outputImage').src = null;
+            document.getElementById('outputImage').src = "http://project-wikisite-835.azurewebsites.net/Image/Get?id=00000000-0000-0000-0000-000000000000";
         }
-        document.getElementById("e-fileinfo").innerHTML = [
-            str
-        ].join("<br>");
+        document.getElementById("exepthionInfo").innerHTML = [str].join("<br>");
     }
-    document.getElementById("uploadInput").addEventListener("change", updateSize);
+
+    function handleFileSelect(evt) {
+        var files = evt.target.files,
+            file = file[0];
+        if (!file.type.match("image.*")) {
+            str = "Не-не-не, только картинки";
+            //document.getElementById('outputImage').src = "/Image/Get?id=00000000-0000-0000-0000-000000000000";
+            document.getElementById('exepthionInfo').innerHTML = [str].join('');
+            files.file[0] = null;
+        } else {
+            var reader = new FileReader();
+            reader.onload = (function (theFile) {
+                return function (e) {
+                    document.getElementById('outputImage').src = e.target.result;
+                };
+            })(f);
+            reader.readAsDataURL(file);
+        }
+    }
+    document.getElementById('uploadInput').addEventListener('change', handleFileSelect, false);
 });
